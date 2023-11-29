e176226051bbfbc97a638896a58442d693d41a9c
comispi <77409412+Micuo@users.noreply.github.com>
2023-11-29 02:01:40 +0000

    COMMITTT

---
+++ hello_world/target/sbf-solana-solana/release/deps/hello_world-c9c834cc82992a43.d
@@ -0,0 +1,7 @@
+/workspaces/sbc-workspace/hello_world/target/sbf-solana-solana/release/deps/hello_world-c9c834cc82992a43.rmeta: src/lib.rs
+
+/workspaces/sbc-workspace/hello_world/target/sbf-solana-solana/release/deps/libhello_world-c9c834cc82992a43.rlib: src/lib.rs
+
+/workspaces/sbc-workspace/hello_world/target/sbf-solana-solana/release/deps/hello_world-c9c834cc82992a43.d: src/lib.rs
+
+src/lib.rs:
